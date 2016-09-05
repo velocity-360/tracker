28c311e340d2f66700042ded9a31a8e60aef4364
fullstack360 <info@fullstack360.com>
2016-09-05 17:45:43 -0400

    modal

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Modal } from 'react-bootstrap'
 
 class App extends Component {
 
 	constructor(props, context){
 		super(props, context)
 		this.showLogin = this.showLogin.bind(this)
+		this.hideLogin = this.hideLogin.bind(this)
 		this.state = {
+			showModal: false
 
 		}
 	}
@@ -14,12 +17,35 @@ class App extends Component {
 	showLogin(event){
 		event.preventDefault()
 		console.log('Show Login')
+		this.setState({
+			showModal: true
+		})
+
+	}
+
+	hideLogin(){
+		this.setState({
+			showModal: false
+		})
 	}
 
 	render(){
 		return (
 			<div>
 				<button onClick={this.showLogin}>Log In</button>
+		        <Modal bsSize="sm" show={this.state.showModal} onHide={this.hideLogin}>
+			        <Modal.Body style={{background:'#f9f9f9', padding:24, borderRadius:3}}>
+			        	<div style={{textAlign:'center'}}>
+				        	<h4>Log In</h4>
+			        	</div>
+			        	<input onChange={this.updateCredentials} id="email" className="form-control" style={{marginBottom:12}} type="text" placeholder="Email" />
+			        	<input onChange={this.updateCredentials} id="password" className="form-control" style={{marginBottom:12}} type="password" placeholder="Password" />
+						<div style={{textAlign:'center', marginTop:24}}>
+							<a onClick={this.login} href="#" className="button button-border button-dark button-rounded button-large noleftmargin">Log In</a>
+						</div>
+			        </Modal.Body>
+		        </Modal>
+
 			</div>
 		)
 	}
